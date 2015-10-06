fa1006f24baa60ca828f8c1700b5d212d5ad6e3e
Bernie Colita <kittywebworks@gmail.com>
2015-10-07 00:49:24 +0800

    cc

--- footer.php
+++ footer.php
@@ -15,35 +15,6 @@
 
 <footer>  
 
-    <div class="pre-footer">
-        <div class="container">   
-            <div class="row">  
-                <div class="col col-sm-12 col-md-12">   
-     <ul class="list-unstyled  list-inline">
-        
-        <li data-uk-scrollspy="{cls:'uk-animation-slide-top', repeat: true, delay:300}"><img alt="" src="images/logo-a-suntrust.png"></li>
-         <li data-uk-scrollspy="{cls:'uk-animation-slide-top', repeat: true, delay:600}"><img alt="" src="images/logo-b-fifth-third-bank.png"></li>
-         <li data-uk-scrollspy="{cls:'uk-animation-slide-top', repeat: true, delay:900}"><img alt="" src="images/logo-c-lcd.png"></li>
-         <li data-uk-scrollspy="{cls:'uk-animation-slide-top', repeat: true, delay:1200}"><img alt="" src="images/logo-d-jpmorgan.png"></li>
-         <li data-uk-scrollspy="{cls:'uk-animation-slide-top', repeat: true, delay:1500}"><img alt="" src="images/logo-e-bbnt.jpg"></li>
-         <li data-uk-scrollspy="{cls:'uk-animation-slide-top', repeat: true, delay:1800}"><img alt="" src="images/logo-f-wu.png"></li>
-    </ul>
-                    </div><!-- /.col12-->
-                </div><!-- /.row -->
-            </div><!-- /.container -->
-    </div><!-- /.pre-footer -->
-    
-    <div class="copyright">
-        <div class="container">   
-            <div class="row">  
-                <div class="col col-sm-12 col-md-12">   
-                    <p>
-                        © 2015, Northwest Association for Financial Professionals
-                    </p>
-                </div><!-- /.col12-->
-            </div><!-- /.row -->
-        </div><!-- /.container -->
-    </div><!-- /.copyright -->
-
+ 
 
 </footer><!-- /footer -->
--- index.php
+++ index.php
@@ -5,39 +5,11 @@ include('header.php'); // Include pulls another file into the page, and sticks i
  
 
 <div class="row">  
-    <div class="col col-sm-12 col-md-7">   
-    <h1>WELCOME TO THE AFP OF NORTHWEST</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam justo orci,
-placerat in elit vitae, elementum mollis turpis. Maecenas id nulla vitae nisl
-rutrum fermentum.</p>
 
-<p>Pellentesque eu ante ut risus auctor aliquam a a tortor. Sed ac consectetur
-lectus, in lobortis nisi. Nullam eu lectus lacus. Integer feugiat viverra justo, eu
-rhoncus ante ornare sed. Donec ac nisi molestie, mollis urna et, imperdiet leo.</p>
+    
+</div><!-- /.row -->
+    
 
-<p>Praesent hendrerit tristique consectetur. Pellentesque habitant morbi
-tristique senectus et netus et malesuada fames ac turpis egestas. Praesent in
-dapibus sem. Sed posuere magna non orci feugiat varius. </p>
-</div><!-- /.col7-->
-        <div class="col col-sm-12 col-md-5">   
-        <img class="img-responsive" alt="" src="<?=$base;?>images/feature-home.jpg">
-    </div><!-- /.col5-->
-    
-    
-    </div><!-- /.row -->
-    
-    <br>
-    <div class="row">  
-        <div class="col col-sm-12 col-md-12">   
-            <h1>CALENDAR OF EVENTS</h1>
-            <div class="row">  
-            <div class="bc-calendar">
-                <? event_feed($event_options); ?>
-            </div><!-- /.bc-calendar -->
-                </div><!-- /.row -->
-        </div><!-- /.col12-->
-    </div><!-- /.row -->
-    
 
 
 
