cf48906cf06f2a4830b4baf5e33a62e6e3f647f0
Bernie Colita <kittywebworks@gmail.com>
2015-10-07 19:29:55 +0800

    cc

--- camc.sh
+++ camc.sh
@@ -1,5 +1,5 @@
 cd /users/haroldking/desktop/bc-dev/camc
 git add .
 git commit -m "cc"
-git ftp init --user camc --passwd c5a2m8c5FM! ftp://67.225.182.242/public_html/redesign
+git ftp push --user camc --passwd c5a2m8c5FM! ftp://67.225.182.242/public_html/redesign
 echo "Upload Success!"
