1b3b5cc33b0a5147ff0360b17344f7221bcb338b
Bernie Colita <kittywebworks@gmail.com>
2015-10-08 23:33:06 +0800

    cc

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ $home = 'Y'; // This variable designates this as the index page, and tells the h
 include('header.php'); // Include pulls another file into the page, and sticks it on where the include is called.
 ?>
  
-
+<div class="bc-slidearea">
     <div class="row">  
-        <div class="bc-slidearea">
+        
     <div class="col col-sm-12 col-md-8">   
         
             <?php  echo quick_slideshow('FD1', 7000); ?> 
@@ -14,9 +14,9 @@ include('header.php'); // Include pulls another file into the page, and sticks i
     <div class="col col-sm-12 col-md-4">   
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
     </div><!-- /.col3-->    
-            </div><!-- /.slide-area -->
+            
         </div><!-- /.row -->
-
+</div><!-- /.slide-area -->
 
 
 
